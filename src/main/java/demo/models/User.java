fc60f6280a4c37728dacfbfe7207d9c77b133ac0
unknown <vugge.m@husky.neu.edu>
2018-09-27 17:50:57 -0400

    final commit

--- src/main/java/demo/models/User.java
+++ src/main/java/demo/models/User.java
@@ -12,11 +12,6 @@ public class User {
 	private String email;
 	private String password;
 	
-	public User(String email, String password) {
-		this.email = email;
-		this.password = password;
-	}
-	
 	
 	public int getId() {
 		return id;
--- src/test/java/demo/RegisterTesting.java
+++ src/test/java/demo/RegisterTesting.java
@@ -9,11 +9,11 @@ import demo.controllers.UserController;
 import demo.models.User;
 
 public class RegisterTesting {
-	User user = new User("abc", "xyz");
+	User user = new User();
 	@Test
 	public void test1() {
 		UserController u = new UserController();
-		User user1 = (User)u.createUser("", new User("abc1", "xyz1")).getBody();
+		User user1 = (User)u.createUser("", user).getBody();
 		assertEquals(user1.getEmail(), "abc1");
 	}
 }
