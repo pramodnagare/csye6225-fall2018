f123ca8a30a838063f9275923994e58ca5cb803e
harsh shah <shah.harsh3@northeastern.edu>
2018-09-27 17:47:50 -0400

    adding asg 2

---
+++ app/infrastructure/aws/cloudformation/csye6225_aws_cf_create_stack_template.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $# -lt 1 ]; then
+  echo "Kindly provide stack name! Try Again."
+  exit 1
+fi
+
+##Creating Stack
+echo "Creating Stack $1"
+response = $(aws cloudformation create-stack --stack-name "$1" --template-body file://csye6225_cf_networking_template_only.json)
+echo "Waiting for Stack $1 to be created"
+aws cloudformation wait stack-create-complete --stack-name $1
+echo "stack $1 is created"
