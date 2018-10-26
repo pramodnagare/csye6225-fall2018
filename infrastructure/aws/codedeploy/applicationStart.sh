95fd6f172249f6ab6adea317a471d649ce0f0eb4
harshshah1291993 <43117369+harshshah1291993@users.noreply.github.com>
2018-10-26 19:51:07 -0400

    Update applicationStart.sh

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sudo systemctl stop tomcat.service
-cd /
+#sudo systemctl stop tomcat.service
+sudo systemctl start tomacat.service
+#cd /
 source env.sh
-cd /opt/tomcat
-java -jar demo-0.0.1-SNAPSHOT.war
+#cd /opt/tomcat
+#java -jar demo-0.0.1-SNAPSHOT.war
