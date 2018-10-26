7ae56672a18978a4f5107c1f627a3190a2245396
pramodnagare <37279055+pramodnagare@users.noreply.github.com>
2018-10-26 01:02:41 -0400

    Update applicationStart.sh

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -2,5 +2,6 @@
 sudo systemctl start tomcat.service
 cd /
 source env.sh
-sudo cd /opt/tomcat/webapps
-sudo java -jar demo-0.0.1-SNAPSHOT.war
+sudo -s
+cd /opt/tomcat/webapps
+java -jar demo-0.0.1-SNAPSHOT.war
