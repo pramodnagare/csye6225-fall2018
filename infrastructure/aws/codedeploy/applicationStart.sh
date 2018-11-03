769dbc53d72d28bace70c28472fb6d48c95d4443
pramodnagare <nagare.p@husky.neu.edu>
2018-11-02 23:50:11 -0400

    Assignment 7 updated

--- infrastructure/aws/codedeploy/applicationStart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#sudo systemctl stop tomcat.service
-sudo cd /
-source env.sh
-sudo systemctl start tomcat.service
-#cd /
-#source env.sh
-#cd /opt/tomcat
-#java -jar demo-0.0.1-SNAPSHOT.war
