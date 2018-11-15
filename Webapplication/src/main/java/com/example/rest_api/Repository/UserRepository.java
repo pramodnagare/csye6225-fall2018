c61b030f9acbaab82421202aea1af2d57bb78c5e
pramodnagare <nagare.p@husky.neu.edu>
2018-11-15 16:13:54 -0500

    Assignment 7&8 updates!

--- Webapplication/src/main/java/com/example/rest_api/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.rest_api.Repository;
-
-import com.example.rest_api.Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,String>{
-
-}
