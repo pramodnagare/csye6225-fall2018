21bba862e76eaed44b8185669a96fac91daec0e7
harsh shah <shah.harsh3@northeastern.edu>
2018-11-20 00:26:49 -0500

    asdas

--- Webapplication/src/main/java/com/example/webapp/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.webapp.Repository;
-
-import com.example.webapp.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,String>{
-
-}
