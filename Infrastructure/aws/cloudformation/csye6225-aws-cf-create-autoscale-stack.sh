61cfb8131668cf9372f512e5db388e1b9b4c69f4
Pramod Nagare <nagare.p@husky.neu.edu>
2018-11-25 06:05:21 -0500

    Assignment 9 updated

---
+++ Infrastructure/aws/cloudformation/csye6225-aws-cf-create-autoscale-stack.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+##Check if enough arguements are passed
+if [ $# -lt 1 ]; then
+  echo "Kindly provide stack name! Try Again."
+  exit 1
+fi
+
+stackname=$1
+domainName=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
+hostedzone=${domainName::-1}
+#s3domainName="$hostedzone.csye6225.com"
+#echo "S3 Domain: $s3domainName"
+echo "HostedZone: $hostedzone"
+
+certARN=$(aws acm list-certificates --query CertificateSummaryList[].CertificateArn  --output text)
+echo "$certARN"
+
+HZID=$(aws route53 list-hosted-zones --query 'HostedZones[0].Id' --output text)
+HZID=${HZID#*e/}
+echo $HZID
+
+export dynamoDBName=csye6225
+export DBSCHEMA=csye6225
+export DBPASSWORD=csye6225password
+export DBUSERNAME=csye6225master
+
+##Creating Stack
+echo "Creating Stack $stackname"
+response=$(aws cloudformation create-stack --stack-name "$stackname" --template-body file://csye6225-cf-autoscale-template.json --parameters ParameterKey=hostedzone,ParameterValue=$hostedzone ParameterKey=hzid,ParameterValue=$HZID ParameterKey=dynamoDBName,ParameterValue=$dynamoDBName ParameterKey=certARN,ParameterValue=$certARN ParameterKey=DBSCHEMA,ParameterValue=$DBSCHEMA ParameterKey=DBPASSWORD,ParameterValue=$DBPASSWORD ParameterKey=DBUSERNAME,ParameterValue=$DBUSERNAME)
+
+
+if [ $? -eq 0 ]; then
+    echo "Waiting for Stack $stackname to be created"
+    echo "$response"
+    aws cloudformation wait stack-create-complete --stack-name $stackname
+    echo "stack $stackname is created"
+else
+	echo "Error in creating of stack"
+	echo $response
+fi;
+
