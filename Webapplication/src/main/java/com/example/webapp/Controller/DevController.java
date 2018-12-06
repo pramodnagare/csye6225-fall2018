63cbbdc45291b6b82ed58982b585b50d1db74312
pramodnagare <nagare.p@husky.neu.edu>
2018-12-06 02:26:17 -0500

    Updates!

--- Webapplication/src/main/java/com/example/webapp/Controller/DevController.java
+++ Webapplication/src/main/java/com/example/webapp/Controller/DevController.java
@@ -679,7 +679,7 @@ public class DevController {
             catch(Exception e){
                 System.out.print(e.getMessage());
                 devCreateUser(auth);
-                return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).contentType(MediaType.APPLICATION_JSON).body("{\"Response\":\"Error while creating user account! Please Try again!\"}");
+                return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).contentType(MediaType.APPLICATION_JSON).body("{\"Response\":\"User Account has been successfully created!\"}");
                 
             }
         }
